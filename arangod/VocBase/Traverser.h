7258553d37063e9e8559b2fad73e3eb6732e77b8
Michael Hackstein <michael@arangodb.com>
2016-12-19 14:59:08 +0100

    Added reverseLookupInfo to ShortestPathOptions.

--- arangod/VocBase/Traverser.h
+++ arangod/VocBase/Traverser.h
@@ -50,6 +50,10 @@ class Query;
 }
 namespace traverser {
 
+#ifdef USE_ENTERPRISE
+class SmartGraphConstDistanceFinder;
+#endif
+
 struct TraverserOptions;
 
 class ShortestPath {
@@ -60,6 +64,10 @@ class ShortestPath {
       arangodb::velocypack::Slice, arangodb::velocypack::Slice,
       arangodb::basics::VelocyPackHelper::VPackStringHash,
       arangodb::basics::VelocyPackHelper::VPackStringEqual, ShortestPath>;
+
+#ifdef USE_ENTERPRISE
+  friend class arangodb::traverser::SmartGraphConstDistanceFinder;
+#endif
 
  public:
   //////////////////////////////////////////////////////////////////////////////
--- arangod/VocBase/TraverserOptions.h
+++ arangod/VocBase/TraverserOptions.h
@@ -156,6 +156,10 @@ struct BaseTraverserOptions {
 
   bool evaluateExpression(aql::Expression*, arangodb::velocypack::Slice varValue) const;
 
+  void injectLookupInfoInList(std::vector<LookupInfo>&, aql::Ast* ast,
+                              std::string const& collectionName,
+                              std::string const& attributeName,
+                              aql::AstNode* condition);
 };
 
 struct TraverserOptions : public BaseTraverserOptions {
@@ -238,6 +242,7 @@ struct ShortestPathOptions : public BaseTraverserOptions {
 
   double _defaultWeight;
   std::string _weightAttribute;
+  std::vector<LookupInfo> _reverseLookupInfos;
 
  public:
 
@@ -275,6 +280,11 @@ struct ShortestPathOptions : public BaseTraverserOptions {
   /// @brief Build a velocypack containing all relevant information
   ///        for DBServer traverser engines.
   void buildEngineInfo(arangodb::velocypack::Builder&) const override;
+
+  /// @brief Add a lookup info for reverse direction
+  void addReverseLookupInfo(aql::Ast* ast, std::string const& collectionName,
+                            std::string const& attributeName,
+                            aql::AstNode* condition);
 };
 
 }
